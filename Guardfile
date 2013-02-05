e9b9519f70ddff72cf2dfbb9d33a206c2352536d
Michael Brodhead <mkb@orthogonal.org>
2013-02-05 01:06:50 -0800

    disable realweb since we don't need it yet

--- Guardfile
+++ Guardfile
@@ -1,7 +1,7 @@
 
 guard 'rspec', :cli => "--color --format nested" do
   watch(%r{^spec/(.+)_spec\.rb$}) { |m| "spec/#{m[1]}_spec.rb" }
-  watch(%r{^lib/(.+)\.rb$})       { |m| "spec/#{m[1]}_spec.rb" }
+  watch(%r{^lib/(.+)\.rb$})       { |m| "spec/lib/#{m[1]}_spec.rb" }
   watch(%r{^spec/support/*})      { 'spec' }
   watch('spec/spec_helper.rb')    { "spec" }
 end
--- spec/lib/unnamed/connector_spec.rb
+++ spec/lib/unnamed/connector_spec.rb
@@ -3,14 +3,14 @@ require 'realweb'
 require 'unnamed/connector'
 
 describe Unnamed::Connector do
-  before :all do
-    fake_server = File.expand_path('./spec/support/fake_working_service.ru')
-    @server = RealWeb.start_server_in_thread(fake_server)
-  end
+  # before :all do
+  #   fake_server = File.expand_path('./spec/support/fake_working_service.ru')
+  #   @server = RealWeb.start_server_in_thread(fake_server)
+  # end
 
-  after :all do
-    @server.stop
-  end
+  # after :all do
+  #   @server.stop
+  # end
 
   pending 'error recording'
   # need to support HMAC
